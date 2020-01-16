b4f85961705e0cc89a58276adc31ddbefd2614d1
sen4senan <bbabusenan@gmail.com>
2020-01-16 13:02:51 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dynamic Editors, Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div> 
   );
 }
 
